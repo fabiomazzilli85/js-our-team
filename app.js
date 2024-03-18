dcd651be642862a6f4144266f80cbf396f4dd270
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-18 14:17:27 +0100

    Creata la funzione JS. Mandato a schermo il risultato in console e nella pagina HTML. Inseriti commenti.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Questo è l'Array contente la lista delle persone che compongono il nostro team.
-
 const personale = [
     {
       nome: "Wayne Barnett",
@@ -34,7 +33,46 @@ const personale = [
   ];
   
 
-  // Mi accerto che la console del browser restituisca i valori che presumo di visualizzare
+// Mi accerto che la console del browser restituisca i valori che presumo di visualizzare.
   console.log(personale[0].nome); // Ok
   console.log(personale[2].ruolo);  // Ok
   console.log(personale[1].foto); // Ok
+
+
+// Visualizzo le stesse informazioni riportate nella console del browser nella pagina HTML.
+// 1. Collego l'esecuzione di questo script al div con id '''team'' nella pagina HTML.
+// 2. Creo una funzione ''visualizzaTeam''.
+// 3. Uso il metodo forEach (fonte Chat GPT)
+// 4. Creo l'elemento ''p''  che contiene il Nome della persona (${persona.nome}).
+// 5. Esattamente come il punto 4, creo un ''p'' in cui è specificato il ruolo all'interno del team.
+// 6. Faccio la stessa cosa con le foto delle quali ho solo il collegamento.
+// 7. Eseguo ilo return della funzione, ovvero ''visualizzaTeam()''.
+const container = document.getElementById('team');
+function visualizzaTeam() {
+    
+    personale.forEach(persona => {
+          const personaDiv = document.createElement('div');
+      
+    
+      const nomeElement = document.createElement('p');
+      nomeElement.textContent = `Nome: ${persona.nome}`;
+      personaDiv.appendChild(nomeElement);
+  
+    
+      const ruoloElement = document.createElement('p');
+      ruoloElement.textContent = `Ruolo: ${persona.ruolo}`;
+      personaDiv.appendChild(ruoloElement);
+  
+    
+      const fotoElement = document.createElement('img');
+      fotoElement.src = persona.foto;
+      fotoElement.alt = persona.nome;
+    
+      personaDiv.appendChild(fotoElement);
+  
+
+      container.appendChild(personaDiv);
+    });
+  }
+  
+  visualizzaTeam();
